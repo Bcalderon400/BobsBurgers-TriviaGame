90b772c54b8b11e78e3970e4aed79463d1b7d37f
Brian <briancalderon200@gmail.com>
2019-07-15 00:20:20 -0700

    started over again, testing new methods and new variables

---
+++ assets/javascript/questions.js
@@ -0,0 +1,17 @@
+var quizQuestions = {
+    {
+        questions: 'What is my full name?',
+        choices: ['brian calderon', 'karen calderon', 'aiden calderon', 'elise calderon'],
+        correctAnswers: 'brian calderon',
+    },
+    {
+        questions: 'Fav sport?',
+        choices: ['football', 'hockey', 'soccer', 'tennis'],
+        correctAnswers: 'soccer',
+    },
+    {
+        questions: 'what town did you live?',
+        choices: ['fresno', 'dinuba', 'yuba city', 'orosi'],
+        correctAnswers: 'dinuba',
+    },
+}
